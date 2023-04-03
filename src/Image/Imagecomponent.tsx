67e4c9b2f4ad5910f3d30f01d390b8fe7f5c3fed
Akash Tandekar <akashta@cybage.com>
2023-04-03 17:32:56 +0530

    All unwanted code has been removed

--- src/Image/Imagecomponent.tsx
+++ src/Image/Imagecomponent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { ImageContainer } from "./ImageStyle";
 
-export default function Imagecomponent({ imgdata }) {
+export default function Imagecomponent({ imgdata }:{imgdata:string}) {
   return (
     <div>
       <ImageContainer src={imgdata} alt="img"></ImageContainer>
